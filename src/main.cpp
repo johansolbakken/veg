de6fe6ae423388227e5151faea731c5c8d0b8c09
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-22 21:35:34 +0100

    Try to access api

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cpr/cpr.h>
+#include "json.h"
 
 void get_road_network()
 {
-    cpr::Response r = cpr::Get(cpr::Url{"https://nvdbapiles-v3.atlas.vegvesen.no/"});
+    cpr::Response r = cpr::Get(cpr::Url{"https://vegkart.atlas.vegvesen.no"});
 
-    std::cout << r.url << std::endl;                    // http://www.httpbin.org/get
-    std::cout << r.status_code << std::endl;            // 200
-    std::cout << r.header["content-type"] << std::endl; // application/json
     std::cout << r.text << std::endl;
 }
 
